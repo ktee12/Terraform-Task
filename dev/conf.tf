42a042ddc0d4551ff3b26f823e996a7c8c9a9d8e
Adekunle Aderibigbe <aderibigbe.adekunle21@gmail.com>
2024-01-29 10:19:12 +0000

    Configured backend for code collaboration using S3 bucket and DynamoDB

--- dev/conf.tf
+++ dev/conf.tf
@@ -8,6 +8,17 @@ terraform {
       version = "~> 4.9.0"
     }
   }
+
+  backend "s3" {
+    # Backend s3 bucket configuration
+    bucket = "sheffied-devOps-dev"
+    key = "terraform-task/terraform.tfstate"
+    region = "eu-west-2"
+    # DynamoDB table configuration for user locking
+    dynamodb_table = "terraform-task-dev-lock"
+    encrypt = true
+    
+  }
 }
 
 provider "aws" {
--- prod/conf.tf
+++ prod/conf.tf
@@ -8,6 +8,17 @@ terraform {
       version = "~> 4.9.0"
     }
   }
+
+  backend "s3" {
+    # Backend s3 bucket configuration
+    bucket = "sheffied-devOps-prod"
+    key = "terraform-task/terraform.tfstate"
+    region = "eu-west-2"
+    # DynamoDB table configuration for user locking
+    dynamodb_table = "terraform-task-prod-lock"
+    encrypt = true
+    
+  }
 }
 
 provider "aws" {
